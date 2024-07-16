4094939d86e425f114da27c4d105d2ae34c63db3
Faizal Nursaori <faizalrahmatn@gmail.com>
2024-07-16 21:49:48 +0700

    fix: change minor types

--- apps/api/src/controllers/event.controller.ts
+++ apps/api/src/controllers/event.controller.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '@/prisma';
 
 export const createEvent = async (req: Request, res: Response) => {
   const {
